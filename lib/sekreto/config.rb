682b220dc32fdb196663b73cfbaa4425afcfd8e1
Chavez <matthew@el-chavez.me>
2018-05-01 11:25:37 -0400

    feat: Fallback on exceptions
    - Use fallback on exception
    - Add logger to config class
    - Add specs for exception happening

--- lib/sekreto/config.rb
+++ lib/sekreto/config.rb
@@ -1,15 +1,19 @@
+require 'logger'
+
 module Sekreto
   class Config
     attr_accessor :prefix
     attr_accessor :is_allowed_env
     attr_accessor :fallback_lookup
     attr_accessor :secrets_manager
+    attr_accessor :logger
 
     def initialize
       @prefix = 'secrets'
       @is_allowed_env = -> { true }
       @fallback_lookup = ->(secret_id) { ENV[secret_id] }
       @secrets_manager = nil
+      @logger = Logger.new(STDOUT)
     end
   end
 end
